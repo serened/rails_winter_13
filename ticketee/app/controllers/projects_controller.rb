5d1fedeeeb02195872e7f6a38dc71648c3c9c478
serened <serened@gmail.com>
2013-01-21 11:33:15 -0800

    add validation to ensure names are specified when creating projects

--- ticketee/app/controllers/projects_controller.rb
+++ ticketee/app/controllers/projects_controller.rb
@@ -9,12 +9,13 @@ class ProjectsController < ApplicationController
 
 	def create 
 		@project = Project.new(params[:project]) 
-			if @project.save
-				flash[:notice] = "Project has been created."
-				redirect_to @project 
-			else
-			# nothing, yet end
-			end
+		if @project.save
+			flash[:notice] = "Project has been created."
+			redirect_to @project 
+		else
+			flash[:alert] = "Project has not been created." 
+			render :action => "new"
+		end
 	end
 
 	def show 
