83960b148a49001916030832655976b81842653c
serened <serened@gmail.com>
2013-01-21 11:08:12 -0800

    creation of projects feature complete

---
+++ ticketee/spec/integration/creating_projects_spec.rb
@@ -0,0 +1,36 @@
+# require 'spec_helper'
+
+# feature 'Creating Projects' do
+#   before do
+#     visit '/'
+#     click_link 'New Project'
+#   end
+#   scenario "can create a project" do
+#     fill_in 'Name', :with => 'TextMate 2' 
+#     fill_in 'Description', :with => "A text-editor for OS X"
+#     click_button 'Create Project'
+#     page.should have_content('Project has been created.')
+#     project = Project.find_by_name("TextMate2")
+#     page.current_url.should == project_url(project)
+#     title = "TextMate2 - Projects - Ticketee"
+#     find("title").should have_content(title)
+#   end
+#   scenario "can not create a project without a name" do
+#     click_button 'Create Project'
+#     page.should have_content("Project has not been created.")
+#     page.should have_content("Name can't be blank")
+#   end
+# end
+
+require 'spec_helper'
+
+feature 'Creating Projects' do 
+  scenario "can create a project" do
+    visit '/' 
+    click_link 'New Project' 
+    fill_in 'Name', :with => 'TextMate 2' 
+    fill_in 'Description', :with => "A text-editor for OS X" 
+    click_button 'Create Project' 
+    page.should have_content('Project has been created.')
+  end 
+end
