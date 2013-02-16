3d2513ad95ea138afc4d682f1302a22953ed137b
serened <serened@gmail.com>
2013-02-16 14:36:04 -0800

    add ability to edit and update users

--- ticketee/app/controllers/admin/users_controller.rb
+++ ticketee/app/controllers/admin/users_controller.rb
@@ -1,10 +1,18 @@
 class Admin::UsersController < Admin::BaseController
+  	before_filter :find_user, :only => [:show, :edit, :update, :destroy]
+
   	def index
   	@users = User.all(:order => "email")
   	end
   	
+  	def show 
+  	end
+  	
   	def new 
   	@user = User.new
+	end
+
+	def edit 
 	end
 
 	def create 
@@ -16,5 +24,26 @@ class Admin::UsersController < Admin::BaseController
 				flash.now[:alert] = "User has not been created."
 				render :action => "new" 
 			end
+	end
+
+	def update
+		@user.skip_reconfirmation!
+		if params[:user][:password].blank? 
+			params[:user].delete(:password) 
+			params[:user].delete(:password_confirmation)
+		end
+		
+		if @user.update_attributes(params[:user], :as => :admin)
+			flash[:notice] = "User has been updated." 
+			redirect_to admin_users_path
+		else 
+			flash[:alert] = "User has not been updated." 
+			render :action => "edit"
+		end 
+	end
+
+	private 
+	def find_user
+		@user = User.find(params[:id]) 
 	end
 end
