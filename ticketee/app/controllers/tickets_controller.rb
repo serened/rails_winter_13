980a4d908025619c94446f9e5603e4c3fe0bc18b
serened <serened@gmail.com>
2013-02-23 18:30:31 -0800

    restrict ticket updating to those who have permission

--- ticketee/app/controllers/tickets_controller.rb
+++ ticketee/app/controllers/tickets_controller.rb
@@ -3,6 +3,7 @@ before_filter :authenticate_user!
 before_filter :find_project
 before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
 before_filter :authorize_create!, :only => [:new, :create]
+before_filter :authorize_update!, :only => [:edit, :update]
 
 	def new
 		@ticket = @project.tickets.build
@@ -44,18 +45,24 @@ before_filter :authorize_create!, :only => [:new, :create]
 
 	private
 	def find_project
-		@project = Project.for(current_user).find(params[:project_id])
-		rescue ActiveRecord::RecordNotFound
-			flash[:alert] = "The project you were looking for could not be found."
-			redirect_to root_path
-	end
-	def find_ticket
+      @project = Project.for(current_user).find(params[:project_id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking for could not be found."
+      redirect_to root_path
+    end
+    def find_ticket
       @ticket = @project.tickets.find(params[:id])
     end
     def authorize_create!
-    	if !current_user.admin? && cannot?("create tickets".to_sym, @project)
-		flash[:alert] = "You cannot create tickets on this project."
-		redirect_to @project
-		end
-	end
+      if !current_user.admin? && cannot?("create tickets".to_sym, @project)
+        flash[:alert] = "You cannot create tickets on this project."
+        redirect_to @project
+      end
+    end
+    def authorize_update!
+      if !current_user.admin? && cannot?("edit tickets".to_sym, @project)
+        flash[:alert] = "You cannot edit tickets on this project."
+        redirect_to @project
+      end
+    end
 end
--- ticketee/spec/integration/editing_tickets_spec.rb
+++ ticketee/spec/integration/editing_tickets_spec.rb
@@ -1,39 +1,37 @@
 require 'spec_helper'
 
 feature "Editing tickets" do
-	# let!(:project) { Factory(:project) }
-	# let!(:ticket) { Factory(:ticket, :project => project) }
-	let!(:project) { Factory(:project) }
-	let!(:user) { Factory(:confirmed_user) }
-	let!(:ticket) do
-		ticket = Factory(:ticket, :project => project)
-		ticket.update_attribute(:user, user)
-		ticket
-		end
+  let!(:project) { Factory(:project) }
+  let!(:user) { Factory(:confirmed_user) }
+  let!(:ticket) do
+    ticket = Factory(:ticket, :project => project)
+    ticket.update_attribute(:user, user)
+    ticket
+  end
 
-before do
-	define_permission!(user, "view", project)
-	sign_in_as!(user)
-	visit '/'
-	click_link project.name
-	click_link ticket.title
-	click_link "Edit Ticket"
-end
+  before do
+    define_permission!(user, "view", project)
+    define_permission!(user, "edit tickets", project)
+    sign_in_as!(user)
+    visit '/'
+    click_link project.name
+    click_link ticket.title
+    click_link "Edit Ticket"
+  end
 
-scenario "Updating a ticket" do
-	fill_in "Title", :with => "Make it really shiny!"
-	click_button "Update Ticket"
-	page.should have_content "Ticket has been updated."
-	within("#ticket h2") do
-		page.should have_content("Make it really shiny!")
-	end
-	page.should_not have_content ticket.title
-end
+  scenario "Updating a ticket" do
+    fill_in "Title", :with => "Make it really shiny!"
+    click_button "Update Ticket"
+    page.should have_content "Ticket has been updated."
+    within("#ticket h2") do
+      page.should have_content("Make it really shiny!")
+    end
+    page.should_not have_content ticket.title
+  end
 
-scenario "Updating a ticket with invalid information" do
-	fill_in "Title", :with => ""
-	click_button "Update Ticket"
-	page.should have_content("Ticket has not been updated.")
-end
-
+  scenario "Updating a ticket with invalid information" do
+    fill_in "Title", :with => ""
+    click_button "Update Ticket"
+    page.should have_content("Ticket has not been updated.")
+  end
 end
