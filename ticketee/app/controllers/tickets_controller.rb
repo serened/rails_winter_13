23754105a3a3cd502626ab763600b10bb948b9a0
serened <serened@gmail.com>
2013-02-23 16:59:33 -0800

    restrict reading tickets to correct project scope : a bunch of specs now broken :/

--- ticketee/app/controllers/tickets_controller.rb
+++ ticketee/app/controllers/tickets_controller.rb
@@ -1,52 +1,52 @@
 class TicketsController < ApplicationController
+before_filter :authenticate_user!
 before_filter :find_project
 before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
-before_filter :authenticate_user!, :except => [:index, :show]
 
-	def new 
+
+	def new
 		@ticket = @project.tickets.build
 	end
 
-	def edit 
+	def edit
 	end
 
-	def create 
-		@ticket = @project.tickets.build(params[:ticket]) 
+	def create
+		@ticket = @project.tickets.build(params[:ticket])
 		@ticket.user = current_user
 		if @ticket.save
 			flash[:notice] = "Ticket has been created."
 			redirect_to [@project, @ticket]
 		else
 			flash[:alert] = "Ticket has not been created."
-			render :action => "new" 
+			render :action => "new"
 		end
 	end
 
-	def show 
+	def show
 	end
 
-	def update 
+	def update
 		if @ticket.update_attributes(params[:ticket])
 			flash[:notice] = "Ticket has been updated."
-			redirect_to [@project, @ticket] 
+			redirect_to [@project, @ticket]
 		else
 			flash[:alert] = "Ticket has not been updated."
-			render :action => "edit" 
+			render :action => "edit"
 		end
 	end
 
-	def destroy 
+	def destroy
 		@ticket.destroy
 		flash[:notice] = "Ticket has been deleted."
-		redirect_to @project 
+		redirect_to @project
 	end
 
-	private 
+	private
 	def find_project
-		@project = Project.find(params[:project_id]) 
+		@project = Project.for(current_user).find(params[:project_id])
+		rescue ActiveRecord::RecordNotFound
+			flash[:alert] = "The project you were looking for could not be found."
+			redirect_to root_path
 	end
-	def find_ticket 
-		@ticket = @project.tickets.find(params[:id])
-	end
-	
 end
