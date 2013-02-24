de2f4d027f0039118a0b938e89b657510b20d68c
serened <serened@gmail.com>
2013-02-23 17:43:57 -0800

    restrict reading tickets to correct project scope

--- ticketee/app/controllers/tickets_controller.rb
+++ ticketee/app/controllers/tickets_controller.rb
@@ -49,4 +49,7 @@ before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
 			flash[:alert] = "The project you were looking for could not be found."
 			redirect_to root_path
 	end
+	def find_ticket
+      @ticket = @project.tickets.find(params[:id])
+    end
 end
