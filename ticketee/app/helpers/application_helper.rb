44180b52131982b51feaf622397c08959fbfd5ad
serened <serened@gmail.com>
2013-02-23 19:17:57 -0800

    restrict actions in TicketController based on permissions and hide links

--- ticketee/app/helpers/application_helper.rb
+++ ticketee/app/helpers/application_helper.rb
@@ -1,16 +1,21 @@
 module ApplicationHelper
-	def title(*parts) 
-		unless parts.empty? 
-			content_for :title do
-				(parts << "Ticketee").join(" - ") 
-			end
-		end 
-	end
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticketee").join(" - ")
+      end
+    end
+  end
 
-	def admins_only(&block) 
-		yield if current_user.try(:admin?)
-		#appending results directly to output directly compiled by erb
+  def admins_only(&block)
+    yield if current_user.try(:admin?)
+  		#appending results directly to output directly compiled by erb
 		#block.call will yield block called by admins_only block
 		#duplicate because of concat; change to yield
-	end
+
+  end
+
+  def authorized?(permission, thing, &block)
+    block.call if can?(permission.to_sym, thing) || current_user.try(:admin?)
+  end
 end
--- ticketee/spec/integration/deleting_tickets_spec.rb
+++ ticketee/spec/integration/deleting_tickets_spec.rb
@@ -1,30 +1,27 @@
-require 'spec_helper'
+require "spec_helper"
 
-feature 'Deleting tickets' do
-	# let!(:project) { Factory(:project) }
-	# let!(:ticket) { Factory(:ticket, :project => project) }
-	let!(:project) { Factory(:project) }
-	let!(:user) { Factory(:confirmed_user) }
-	let!(:ticket) do
-		ticket = Factory(:ticket, :project => project)
-		ticket.update_attribute(:user, user)
-		ticket
-		end
+feature 'deleting tickets' do
+  let!(:project) { Factory(:project) }
+  let!(:user) { Factory(:confirmed_user) }
+  let!(:ticket) do
+    ticket = Factory(:ticket, :project => project)
+    ticket.update_attribute(:user, user)
+    ticket
+  end
 
-before do
-	define_permission!(user, "view", project)
-	define_permission!(user, "delete tickets", project)
-	sign_in_as!(user)
+  before do
+    define_permission!(user, "view", project)
+    define_permission!(user, "delete tickets", project)
+    sign_in_as!(user)
+    visit '/'
+    click_link project.name
+    click_link ticket.title
+  end
 
-	visit '/'
-	click_link project.name
-	click_link ticket.title
-end
-
-scenario "Deleting a ticket" do
-	click_link "Delete Ticket"
-	page.should have_content("Ticket has been deleted.")
-	page.current_url.should == project_url(project)
-end
+  scenario "Deleting a ticket" do
+    click_link "Delete Ticket"
+    page.should have_content("Ticket has been deleted.")
+    page.current_url.should == project_url(project)
+  end
 
 end
