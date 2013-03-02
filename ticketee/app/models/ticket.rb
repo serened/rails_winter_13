1afac2341829d367d96064d9b2c820727b250572
serened <serened@gmail.com>
2013-03-02 15:15:43 -0800

    users can now upload 3 files at a time

--- ticketee/app/models/ticket.rb
+++ ticketee/app/models/ticket.rb
@@ -1,8 +1,9 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
-  attr_accessible :title, :description, :asset
+  attr_accessible :title, :description, :assets_attributes
   validates :title, :presence => true
   validates :description, :presence => true, :length => { :minimum => 10 }
   belongs_to :user
-  has_attached_file :asset
+  has_many :assets
+  accepts_nested_attributes_for :assets
 end
