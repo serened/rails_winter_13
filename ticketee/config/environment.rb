a66569a9cd265daf64f03e8665b498aec086729f
serened <serened@gmail.com>
2013-03-02 23:09:45 -0800

    provide an 'add another file' link that uses javascript so that users can upload more than one file

--- ticketee/config/environment.rb
+++ ticketee/config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ticketee::Application.initialize!
+
+# Capybara.register_driver :selenium do |app|
+#   Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# end
